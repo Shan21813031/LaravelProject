957ee04f59b56545f5fdbeefd23fceda230ffc7b
Shan21813031 <S.AUK@outlook.com>
2022-04-15 03:41:32 +0100

    Created initial controller and blade files

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'index']);
+
+Route::resource('/Shop', [PostController::class]);
 
 Route::middleware([
     'auth:sanctum',
